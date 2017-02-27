b81cbc7efe8225aad365ab25de65ac2c39ed87cf
dani heriyanto <dani@go-jek.com>
2017-02-27 23:35:30 +0700

    del 2

--- app/src/main/java/com/example/tutorial/tutorialtest/MainActivity.java
+++ app/src/main/java/com/example/tutorial/tutorialtest/MainActivity.java
@@ -29,15 +29,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
     }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
+    
 
     @Override
     protected void onRestart() {
         super.onRestart();
     }
-    
+
 }
